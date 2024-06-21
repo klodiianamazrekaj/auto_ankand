6157b28ea392bbda82eaad3d8618f460c233d2be
Klodiana Mazrekaj <klodianamazrekaj20@gmail.com>
2024-06-21 22:11:47 +0200

    added a message outbox

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cdce0860f25753822862e6755e8eab2f71070f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4ea7f084148781cf337857a6d7430356f823d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
