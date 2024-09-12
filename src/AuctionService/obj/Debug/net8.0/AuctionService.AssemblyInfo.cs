ad35b0cfb53d8206506c254b87b11685c0c4697f
Klodiana Mazrekaj <klodianamazrekaj20@gmail.com>
2024-09-13 00:36:47 +0200

    added an api controller

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b8135e60b4123d1d9b716d563634eab83346e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f68b153e207c7c782a284006430c33459cab681")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
