c60635be148038c33d23811782fddd8791b99068
Klodiana Mazrekaj <klodianamazrekaj20@gmail.com>
2024-09-13 23:13:04 +0200

    started building the search service

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad35b0cfb53d8206506c254b87b11685c0c4697f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa520faef8e6782eca189297a4f3292c193c015e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
