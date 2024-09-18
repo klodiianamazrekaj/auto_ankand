9a02fabed4ca8e07432c86efbe6ed484c2ec2c33
Klodiana Mazrekaj <klodianamazrekaj20@gmail.com>
2024-09-18 19:20:37 +0200

    making register and login functional

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5b79f28a608cb80776a7c953337690813817bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ca3cba4277144aa01c79f4c0fd25b9bcd5fda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
