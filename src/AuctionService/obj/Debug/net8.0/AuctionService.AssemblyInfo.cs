e8c3492507d817ff4d225955ff2c002fcb022cc9
Klodiana Mazrekaj <klodianamazrekaj20@gmail.com>
2024-09-18 20:00:41 +0200

    finished building the identity service and authentication

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ca3cba4277144aa01c79f4c0fd25b9bcd5fda3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a02fabed4ca8e07432c86efbe6ed484c2ec2c33")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
